517ab8fc0e64c3f55a2fc17212ee2ac7c9e8ab04
Prakash8999 <reshavejha@gmail.com>
2023-06-02 02:09:53 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-
+import Summary from './pages/Summary';
+import { Toaster } from 'react-hot-toast';
 function App() {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/summary/:id" element={<Summary />} />
         </Routes>
+        <Toaster />
       </Router>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,56 @@
-import React from 'react'
-import logo from '../logo.svg';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-	//  Template Code
-	return (
-		<>
-			<div className='relative bg z-0' >
-				<section className='flex-col Nunito relative z-10 h-screen w-screen overflow-hidden flex items-center justify-center'>
-					<h1 className='text-4xl text-white font-bold -mt-20 mb-20 w-[50vw] text-center'>A complete boilerplate for your next tailwind and React project!</h1>
-					<div className='flex'>
-						<div className='w-[30vw] items-center justify-center flex flex-col'>
-							<img src={logo} className="h-[20vh]" alt="" />
-							<h2 className='text-2xl font-bold text-white text-center'>ReactJs</h2>
-						</div>
-						<div className='w-[30vw] items-center justify-center flex flex-col'>
-							<img className="h-[20vh]" src="https://camo.githubusercontent.com/bcd4bda49ef6cd9537db065920f4f4f6ac670eae0e0adf2c5133c19b319f1574/68747470733a2f2f627261646c632e67616c6c65727963646e2e76736173736574732e696f2f657874656e73696f6e732f627261646c632f7673636f64652d7461696c77696e646373732f302e322e302f313535383034303536333634392f4d6963726f736f66742e56697375616c53747564696f2e53657276696365732e49636f6e732e44656661756c74" alt="" />
-							<h2 className='text-2xl font-bold text-white text-center'>TailwindCSS</h2>
-						</div>
-					</div>
-				</section>
-			</div>
-		</>
-	)
-}
+  const [show, setShow] = useState();
 
-export default Home
+  useEffect(() => {
+    axios(`https://api.tvmaze.com/search/shows?q=all`, {
+      method: "GET",
+    })
+      .then((response) => {
+        setShow(response.data);
+        // console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <>
+      <div className="grid grid-cols-4 gap-5 p-5  text-white bg-[#252831] bg-opacity-60 ">
+        {show?.map((value, index) => {
+          return (
+            <div
+              key={index}
+              className="flex justify-center items-center flex-col"
+            >
+              <div className="p-3 ">
+                <img
+                  src={value?.show.image.medium}
+                  className=" rounded-lg"
+                  alt=""
+                />
+                <p className="font-semibold">{value?.show.name}</p>
+                <p>{value?.show.language}</p>
+                <hr className="border-1 border-white" />
+                <p>{value?.show.genres.join(", ")} </p>
+              </div>
+              <div>
+                <Link
+                  to={`/summary/${value?.show.id}`}
+                  className=" font-semibold text-lg duration-100 border hover:bg-blue-600 px-3 py-1.5 rounded-lg"
+                >
+                  View Summary
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Home;
